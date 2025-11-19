e1a1aec0d2c82ead15f67df46e5f3fa4d75260c3
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-19 20:55:11 +0200

    finito

--- Primes.java
+++ Primes.java
@@ -1,5 +1,29 @@
 public class Primes {
+    public static boolean isPrime(int number)
+    {
+        boolean prime = true;
+        for (int i=2; i<=number-1; i++)
+        {
+            if (number % i == 0)
+            {
+                prime = false;
+            }
+        }
+        return prime;
+    }
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        int per;
+        int count = 0;
+        
+        for (int i=2; i<=n; i++)
+        {
+            if (isPrime(i) == true)
+            {
+                count++;
+            }
+        }
+        per = (int)(100 * count / n);
+        System.out.println("There are " + count + " primes between 2 and " + n +" (" + per + "% are primes)");
     }
 }
