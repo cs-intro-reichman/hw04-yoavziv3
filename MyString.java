47d77d40b00dbe9d42cfaa57d342c03652db8565
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-20 16:10:02 +0200

    yay

--- MyString.java
+++ MyString.java
@@ -1,10 +1,10 @@
 public class MyString {
     public static void main(String[] args) {
-        System.out.println("Testing lowercase:");
-        System.out.println("UnHappy : " + lowerCase("UnHappy"));
-        System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
-        System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
+        //System.out.println("Testing lowercase:");
+        //System.out.println("UnHappy : " + lowerCase("UnHappy"));
+        //System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
+        //System.out.println("TLV : " + lowerCase("TLV"));
+        //System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
@@ -18,13 +18,37 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lowWord="";
+        for(int i=0; i<str.length(); i++)
+        {
+            if((char)str.charAt(i) >= 65 && (char)str.charAt(i) <= 90)
+            {
+                lowWord = lowWord + (char)(str.charAt(i) + 32);
+            }
+            else
+            {
+                lowWord = lowWord + str.charAt(i);
+            }
+        }
+        return lowWord;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        for(int i=0; i<str1.length(); i++)
+        {
+            int k =i;
+            int t = 0;
+            while (k<str1.length() && t<str2.length() && str1.charAt(k) == str2.charAt(t))
+            {
+                t++;
+                k++;    
+            }
+            if(t == str2.length())
+            {
+                return true;
+            }
+        }
         return false;
     }
 }
